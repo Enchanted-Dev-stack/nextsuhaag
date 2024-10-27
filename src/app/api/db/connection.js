d4f375d6c30de2f682a33c98f3a79383c859646d
Enchanted-Dev-stack <theadarsh7205@gmail.com>
2024-10-27 14:15:27 +0530

    rebuild

--- src/app/api/db/connection.js
+++ src/app/api/db/connection.js
@@ -4,14 +4,12 @@ const connection_string = process.env.MONGO_URI;
 export const connectDb = async () => {
 
     try {
-        const connection = await mongoose.connect(connection_string, {
-            dbName: 'Suhaag-Matrimony',
-        });
+        const connection = await mongoose.connect(connection_string);
 
         console.log('Connected to MongoDB');
         console.log(connection.connection.db.databaseName); // Database name verification
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message || error);
     }
-    
+
 };
